cafa49a61400693204dce38069fc80d10c9f9be4
Un Programador Nace <74442754+UnProgramadorNace@users.noreply.github.com>
2023-12-10 00:05:39 -0500

    :sparkles: feature/rest-services: Implement update and delete methods 1

--- src/main/java/com/school/app/repository/IStudentRepository.java
+++ src/main/java/com/school/app/repository/IStudentRepository.java
@@ -1,9 +1,8 @@
 package com.school.app.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import com.school.app.model.Student;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
-import com.school.app.model.Student;
 
 @Repository
-public interface IStudentRepository extends CrudRepository<Student, Long> {}
+public interface IStudentRepository extends ListCrudRepository<Student, Long> {}
